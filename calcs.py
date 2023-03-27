fd01add207fbc6b4eff4b3bdd6871a4e694bae2f
OleksandrSkr <skrigunaleksandr@gmail.com>
2023-03-27 22:35:54 +0300

    sum 2

--- calcs.py
+++ calcs.py
@@ -1,14 +1,21 @@
-def calcs():
-    from app import operation
-    if operation == "+" :
-        from sum import sum
+from func_tools.sum import sum
+from func_tools.subtruct import subtract
+from func_tools.multiply import multiply
+from func_tools.divide import divide
+resalt = sum(1 , 2)
+print(resalt)
 
-    elif operation == "-" :
-        from subtruct import subtruct
 
-    elif operation == "*" :
-        from multiply import multiply
+#def calcs():
+#    from app import operation
+#    if operation == "+" :
+#        from sum import sum
+#
+#    elif operation == "-" :
+#        from subtruct import subtruct
+
+#    elif operation == "*" :
+#        from multiply import multiply 
     
-    elif operation == "/" :
-        from divide import divide
-        
+#    elif operation == "/" :
+#        from divide import divide
