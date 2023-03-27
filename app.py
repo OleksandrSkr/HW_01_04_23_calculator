e0e9c8b6fcf53f98350b7e8b935f4025d1c22189
OleksandrSkr <skrigunaleksandr@gmail.com>
2023-03-27 20:56:15 +0300

    start

---
+++ app.py
@@ -0,0 +1,16 @@
+is_running = True
+
+while is_running :
+    user_choose = input("Do you want to work wich calc? y / n : ")
+
+    if user_choose == "y" :
+        operation = input("What math operation do you want to perform - '+' , '-' , '*' , '/' , '^' ?")
+        if operation in "+-*/^":
+            from calcs import calcs
+        else: 
+            continue
+    
+    elif user_choose == "n" :
+        is_running = False
+    else :
+        continue
